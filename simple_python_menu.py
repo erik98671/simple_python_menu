9d70ccc3f11c354e44227d66f827e6dcb9f57201
Erik Braun <30779061+erik98671@users.noreply.github.com>
2018-09-28 08:35:04 -0700

    Style update

--- simple_python_menu.py
+++ simple_python_menu.py
@@ -1,27 +1,25 @@
 # Create array of menu items to select from
-menu_items = ['First Menu Item']
+menu_items = ["First Menu Item"]
 
 # Define the menu items
 def get_menu_item():
-    print('Menu:')
-    print()
+    print("Menu:\n")
     for i in range(len(menu_items)):
-        print(str(i + 1) + ' - ' + menu_items[i])
-    print('Q - Quit')
-    print()
-    menu_selection = input('Selection: ')
+        print((i + 1), "-", menu_items[i])
+    print("Q - Quit\n")
+    menu_selection = input("Selection: ")
     print()
     return menu_selection
 
 # Define the main loop
 def main_loop():
-    quit_flag = 'N'
-    while quit_flag == 'N':
+    quit_flag = "N"
+    while quit_flag == "N":
         selection = get_menu_item()
-        if selection == '1':
+        if selection == "1":
             function_1()
-        if selection.upper() == 'Q':
-            quit_flag = 'Y'
+        if selection.upper() == "Q":
+            quit_flag = "Y"
 
 # Define function(s)
 def function_1():
