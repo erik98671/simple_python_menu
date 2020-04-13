924eaffb5f75445f3ce980472fdb384f6de2f7cd
Erik Braun <30779061+erik98671@users.noreply.github.com>
2020-04-13 09:39:29 -0700

    Updates

--- simple_python_menu.py
+++ simple_python_menu.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 # simple_python_menu.py
-# Last updated 202003141015
+# Last Updated 202004130900
 
 # Create array of menu items to select from
 menu_items = ["First Menu Item"]
 
+# Define the main loop
+def main():
+    quit_flag = "N"
+    while quit_flag == "N":
+        selection = get_menu_item()
+        if selection == "1":
+            function_1()
+        if selection.upper() == "Q":
+            quit_flag = "Y"
+
 # Define the menu items
-def get_menu_item(menu_items):
+def get_menu_item():
     print("Menu:\n")
     for i in range(len(menu_items)):
         print((i+1), "-", menu_items[i])
@@ -15,23 +25,10 @@ def get_menu_item(menu_items):
     print()
     return menu_selection
 
-# Define the main loop
-def main(menu_items):
-    print("")
-    quit_flag = "N"
-    while quit_flag == "N":
-        selection = get_menu_item(menu_items)
-        if selection == "1":
-            function_1()
-        if selection.upper() == "Q":
-            quit_flag = "Y"
-
-# Define function
+# Define function_1()
 def function_1():
-    print("Hello World.\n")
     return
 
-# If this program is started on it's own, run the main() function
+# If this program is started on it's own, run the main() function.
 if __name__ == "__main__":
-    main(menu_items)
-    print("Good bye.\n")
+    main()
