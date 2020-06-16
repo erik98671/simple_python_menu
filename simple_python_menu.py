547a8839b7d1bb2cba363af866e61cc68e15363a
Erik Braun <erikbraun@me.com>
2020-06-16 12:53:36 -0700

    Updates

--- simple_python_menu.py
+++ simple_python_menu.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 # simple_python_menu.py
-# Last Updated 202004130900
-
-# Create array of menu items to select from
-menu_items = ["First Menu Item"]
+# Last Updated 202006161300
 
 # Define the main loop
-def main():
+def main(menu_items):
     quit_flag = "N"
     while quit_flag == "N":
-        selection = get_menu_item()
+    
+        selection = get_menu_item(menu_items)
+        
         if selection == "1":
             function_1()
+            
         if selection.upper() == "Q":
             quit_flag = "Y"
 
 # Define the menu items
-def get_menu_item():
+def get_menu_item(menu_items):
+     
     print("Menu:\n")
-    for i in range(len(menu_items)):
-        print((i+1), "-", menu_items[i])
+    
+    for item in menu_items: 
+        print(menu_items.index(item)+1, "-", item)
+
     print("Q - Quit\n")
+
     menu_selection = input("Selection: ")
     print()
+    
     return menu_selection
 
 # Define function_1()
@@ -31,4 +36,9 @@ def function_1():
 
 # If this program is started on it's own, run the main() function.
 if __name__ == "__main__":
-    main()
+    
+    # Create list of menu items to select from
+    menu_items = ["First Menu Item"]
+
+    # Run the main program
+    main(menu_items)
